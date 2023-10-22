56894127bdd3ccfd0c4c9b2c1920f5ab56586368
Antoine Clarman <antoinereims28@gmail.com>
2023-10-22 21:28:38 +0200

    chore: fix pixi.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Application } from 'pixi.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'labrute-angular';
+
+  game = new Application();
 }
